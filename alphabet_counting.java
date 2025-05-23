5e07020400e636f97a8ad716de644d0977dec156
Donggil Wee <coalla7303@gmail.com>
2025-05-23 17:44:38 +0900

    문장 알파벳 갯수 세기

---
+++ alphabet_counting.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class alphabet_counting {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        Set<Character> set = new HashSet<>();
+
+        char[] word = input.toLowerCase().toCharArray();
+
+        for (char w : word) {
+            if (Character.isLetter(w)){
+                set.add(w);
+            }
+        }
+        System.out.println(set.size());
+    }
+}
